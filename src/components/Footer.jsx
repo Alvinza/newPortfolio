01ab834dfea485c9674554a0a2d46df1fe70e1ff
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-11-08 10:52:29 +0200

    Change footer background color from slate to zinc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="text-center p-6 bg-slate-100 text-black">
+  <footer className="text-center p-6 bg-zinc-950 text-black">
     © {new Date().getFullYear()} Alvin Zondi. All rights reserved.
   </footer>
 );
