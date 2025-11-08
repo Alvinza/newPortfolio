a360941909e2f3dcd87a54e2173e12cbc8d3d63e
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-11-08 10:56:09 +0200

    Change footer text color from black to gray

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="text-center p-6 bg-zinc-950 text-black">
+  <footer className="text-center p-6 bg-zinc-950 text-gray-400">
     © {new Date().getFullYear()} Alvin Zondi. All rights reserved.
   </footer>
 );
